4bcab058e5b405f673fd7f6589dbf73188bf02a5
IMRANDIL <aliimranadil2@gmail.com>
2022-03-29 09:22:06 +0530

    YH

--- food-app/components/Navbar.jsx
+++ food-app/components/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import styles from '../styles/Navbar.module.css'
+import Image from 'next/image'
+
+
+
+
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className={styles.container}>
+            <div className={styles.item}>
+
+                <div className={styles.callButton}>
+                    <image src='/img/telephone.png' alt='telephone-img' />
+                </div>
+                <div className={styles.texts}>
+                    <div className={styles.text}>ORDER NOW!</div>
+                    <div className={styles.text}>91 85258XXX</div>
+                </div>
+
+
+
+            </div>
+            <div className={styles.item}></div>
+            <div className={styles.item}></div>
+        </div>
     )
 }
 
