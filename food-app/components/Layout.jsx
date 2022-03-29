2c9252a537ed9af8f2d045df7391b7c0b3642134
IMRANDIL <aliimranadil2@gmail.com>
2022-03-29 09:00:23 +0530

    yh

---
+++ food-app/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Navbar from './Navbar'
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar />
+            Home
+            <Footer />
+        </>
+    )
+}
+
+export default Layout
