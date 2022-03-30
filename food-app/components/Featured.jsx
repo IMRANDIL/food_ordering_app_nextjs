b4f2e2779d42e67883bf2b0418a6d303ae9e24a2
IMRANDIL <aliimranadil2@gmail.com>
2022-03-30 05:37:31 +0530

    yh

--- food-app/components/Featured.jsx
+++ food-app/components/Featured.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/Featured.module.css'
 
@@ -13,8 +14,39 @@ import styles from '../styles/Featured.module.css'
 
 
 const Featured = () => {
+
+
+    const images = [
+        '/img/featured.png',
+        '/img/featured2.png',
+        '/img/featured3.png'
+    ]
+
+
+
+
+
+
+
+
+
     return (
-        <div>Featured</div>
+        <div className={styles.container}>
+            <Image src='/img/arrowl.png' alt='arrowl-img' />
+            <div className={styles.wrapper}>
+                <div className={styles.imgContainer}>
+                    {images.map((img) => (
+                        <Image src={img} alt='featured-img' />
+                    ))}
+
+                </div>
+
+
+
+
+            </div>
+            <Image src='/img/arrowr.png' alt='arrowr-img' />
+        </div>
     )
 }
 
